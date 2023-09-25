00a57fb82a94c2d098bd83317800e13ae0cecff4
Icung <saiful-islam@outlook.co.id>
2023-09-25 18:20:45 +0700

    testing-with-rxtest

--- 16-testing-with-rxtests/projects/starter/Testing/TestingTests/TestingOperators.swift
+++ 16-testing-with-rxtests/projects/starter/Testing/TestingTests/TestingOperators.swift
@@ -36,14 +36,92 @@ import RxTest
 import RxBlocking
 
 class TestingOperators : XCTestCase {
-  var scheduler: TestScheduler!
-  var subscription: Disposable!
+    var scheduler: TestScheduler!
+    var subscription: Disposable!
+    
+    override func setUp() {
+        super.setUp()
+        
+        scheduler = TestScheduler(initialClock: 0)
+    }
+    
+    override func tearDown() {
+        scheduler.scheduleAt(1000) { [weak self] in
+            self?.subscription.dispose()
+        }
+        
+        scheduler = nil
+        
+        super.tearDown()
+    }
+    
+    func testAmb() {
+        let observer = scheduler.createObserver(String.self)
+        
+        let observableA = scheduler.createHotObservable([
+          .next(100, "a"),
+          .next(200, "b"),
+          .next(300, "c"),
+        ])
+        
+        let observableB = scheduler.createHotObservable([
+          .next(90, "1"),
+          .next(200, "2"),
+          .next(300, "3")
+        ])
 
-  override func setUp() {
-    super.setUp()
-  }
+        let ambObservable = observableA.amb(observableB)
+        subscription = ambObservable.subscribe(observer)
+        scheduler.start()
 
-  override func tearDown() {
-    super.tearDown()
-  }
+        let results = observer.events.compactMap {
+          $0.value.element
+        }
+
+        XCTAssertEqual(results, ["1", "2", "3"])
+    }
+    
+    func testFilter() {
+        let observer = scheduler.createObserver(Int.self)
+        
+        let observable = scheduler.createHotObservable([
+            .next(100, 1),
+            .next(200, 2),
+            .next(300, 3),
+            .next(400, 2),
+            .next(500, 1)
+        ])
+        
+        let filterObservable = observable.filter {
+            $0 < 3
+        }
+        
+        scheduler.scheduleAt(0) { [weak self] in
+            self?.subscription = filterObservable.subscribe(observer)
+        }
+        
+        scheduler.start()
+        
+        let resutls = observer.events.compactMap {
+            $0.value.element
+        }
+        
+        XCTAssertEqual(resutls, [1, 2, 2, 1])
+    }
+    
+    func testToArray() {
+        let scheduler = ConcurrentDispatchQueueScheduler(qos: .default)
+        let toArrayObservable = Observable.of(1, 2).subscribeOn(scheduler)
+        
+        let result = toArrayObservable
+            .toBlocking()
+            .materialize()
+        
+        switch result {
+        case .completed(elements: let elements):
+            XCTAssertEqual(elements, [1, 2])
+        case .failed(_, let error):
+            XCTFail(error.localizedDescription)
+        }
+    }
 }
